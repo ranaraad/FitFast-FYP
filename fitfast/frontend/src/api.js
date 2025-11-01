971514f54945409bc40a5e2cd1648e5cf820d98f
Rana Raad <202210376@ua.edu.lb>
2025-11-01 21:58:34 +0200

    created an axios instance to store tokens

---
+++ fitfast/frontend/src/api.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "http://127.0.0.1:8000/api",
+});
+
+// every request: attach Authorization header if we have a token
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("auth_token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  config.headers.Accept = "application/json";
+  return config;
+});
+
+export default api;

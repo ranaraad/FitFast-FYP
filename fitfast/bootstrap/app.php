0af29161dea5739e1af1ae58fb6a417db5b61f28
Ali Murad <202112019@ua.edu.lb>
2025-11-26 19:43:28 +0200

    Finalized Fix

--- fitfast/bootstrap/app.php
+++ fitfast/bootstrap/app.php
@@ -21,7 +21,7 @@ return Application::configure(basePath: dirname(__DIR__))
 
         // Register your custom middleware
         $middleware->alias([
-            'auth' => \App\Http\Middleware\Authenticate::class, // Add this line
+            'auth' => \App\Http\Middleware\Authenticate::class, 
             'cms.access' => \App\Http\Middleware\CheckCMSAccess::class,
             'storeadmin.access' => \App\Http\Middleware\CheckStoreAdminAccess::class,
         ]);

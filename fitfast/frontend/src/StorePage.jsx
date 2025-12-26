87f4f7f9b39a6a3421b89104c90e86f600e67e07
Rana Raad <202210376@ua.edu.lb>
2025-12-26 12:25:26 +0200

    imports

--- fitfast/frontend/src/StorePage.jsx
+++ fitfast/frontend/src/StorePage.jsx
@@ -1,4 +1,5 @@
-import { useParams,useState,useEffect } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import api from "./api";
 
 export default function StorePage() {
